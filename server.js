57c572af829580603947c06d24e78782137b16cc
Joshua deLorimier <TRASHTRASH_HQ@TRASHTRASHPC.localdomain>
2020-01-15 13:55:02 -0500

    d

--- server.js
+++ server.js
@@ -7,11 +7,18 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 4000;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/reactShopping', { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false });
-const connection = mongoose.connection;
-connection.once('open',()=>{
-    console.log('MongoDB database connection established!');
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/reactShopping', { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false });
+// const connection = mongoose.connection;
+// connection.once('open',()=>{
+//     console.log('MongoDB database connection established!');
+// });
+const db = process.env.MONGODB_URI || 'mongodb://localhost:27017/reactShopping';
+mongoose 
+    .connect(db, { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false })
+    .then(() => {
+        console.log('Database Connected!!!!');
+    })
+    .catch (err => console.log(err));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
