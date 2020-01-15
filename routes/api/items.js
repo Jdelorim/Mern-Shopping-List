6e0addc5edc3af811629b8926d459320c7aed62d
Joshua deLorimier <TRASHTRASH_HQ@TRASHTRASHPC.localdomain>
2020-01-15 15:27:21 -0500

    ee

--- routes/api/items.js
+++ routes/api/items.js
@@ -10,5 +10,21 @@ router.get('/', (req, res) => {
         .then(items => res.json(items))
 });
 
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const newItem = new Item({
+        name: req.body.name
+    });
+    newItem.save()
+        .then(item => res.json(item))
+        .catch(err=>console.log(err))
+})
+
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err=> res.status(404).json({ success: false }))
+})
+
 
 module.exports = router;
